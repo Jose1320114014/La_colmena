8d2e4b2b5b358dd976df9521323af0a121514d6e
TyrantYharonsmoke -ム--ム- The <jose1320114014@uptecamac.edu.mx>
2022-06-24 02:14:22 -0500

    git push ejemplo

--- resources/views/catalogo.blade.php
+++ resources/views/catalogo.blade.php
@@ -67,7 +67,7 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12 text-center logo-wrap">
-						<div id="fh5co-logo"><a href="{{route('index')}}">Tasty<span>.</span></a></div>
+						<div id="fh5co-logo"><a href="{{route('index')}}">La colmena<span>.</span></a></div>
 					</div>
 					<div class="col-xs-12 text-center menu-1 menu-wrap">
 						<ul>
@@ -95,8 +95,7 @@
 				<div class="col-md-12 text-center">
 					<div class="display-t js-fullheight">
 						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
-							<h1>See <em>Our</em> Menu</h1>
-							<h2>Brought to you by <a href="http://freehtml5.co/" target="_blank">freehtml5.co</a></h2>
+							<h1>Un vistazo <em>a</em> Nuestros productos</h1>
 						</div>
 					</div>
 				</div>
@@ -108,23 +107,23 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 fh5co-heading animate-box">
-					<h2>Our Delicous Menu</h2>
+					<h2>Aqui una muestra de lo que ofrece nuestra empresa.</h2>
 					<div class="row">
 						<div class="col-md-6">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ab debitis sit itaque totam, a maiores nihil, nulla magnam porro minima officiis! Doloribus aliquam voluptates corporis et tempora consequuntur ipsam, itaque, nesciunt similique commodi omnis. Ad magni perspiciatis, voluptatum repellat.</p>
+							
 						</div>
 					</div>
 				</div>
 				
 				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
 					<div class="fh5co-item animate-box">
-						<img src="images/gallery_1.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/bibipolvo.png" class="img-responsive">
 						<h3>Bake Potato Pizza</h3>
 						<span class="fh5co-price">$20<sup>.50</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
 					</div>
 					<div class="fh5co-item animate-box">
-						<img src="images/gallery_2.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/bolitatamarindo.png" class="img-responsive" >
 						<h3>Bake Potato Pizza</h3>
 						<span class="fh5co-price">$20<sup>.50</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
@@ -132,13 +131,13 @@
 				</div>
 				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
 					<div class="fh5co-item margin_top animate-box">
-						<img src="images/gallery_3.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
-						<h3>Salted Fried Chicken</h3>
+						<img src="images/luckygummysosito.png" class="img-responsive" >
+						<h3>Sed Fried Chicken</h3>
 						<span class="fh5co-price">$19<sup>.00</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
 					</div>
 					<div class="fh5co-item animate-box">
-						<img src="images/gallery_4.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/canasta.png" class="img-responsive" >
 						<h3>Bake Potato Pizza</h3>
 						<span class="fh5co-price">$20<sup>.50</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
@@ -147,13 +146,13 @@
 				<div class="clearfix visible-sm-block visible-xs-block"></div>
 				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
 					<div class="fh5co-item animate-box">
-						<img src="images/gallery_5.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/clorets_2s.png" class="img-responsive" >
 						<h3>Italian Sauce Mushroom</h3>
 						<span class="fh5co-price">$17<sup>.99</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
 					</div>
 					<div class="fh5co-item animate-box">
-						<img src="images/gallery_6.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/cococuadro.png" class="img-responsive" >
 						<h3>Bake Potato Pizza</h3>
 						<span class="fh5co-price">$20<sup>.50</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
@@ -161,13 +160,15 @@
 				</div>
 				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
 					<div class="fh5co-item margin_top animate-box">
-						<img src="images/gallery_7.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/cremino.png" class="img-responsive" >
 						<h3>Fried Potato w/ Garlic</h3>
 						<span class="fh5co-price">$22<sup>.50</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
 					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
 					<div class="fh5co-item animate-box">
-						<img src="images/gallery_8.jpeg" class="img-responsive" alt="Free Restaurant Bootstrap Website Template by FreeHTML5.co">
+						<img src="images/bibipolvo.png" class="img-responsive">
 						<h3>Bake Potato Pizza</h3>
 						<span class="fh5co-price">$20<sup>.50</sup></span>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
@@ -176,7 +177,73 @@
 			</div>
 		</div>
 	</div>
-
+	<div id="fh5co-featured-menu" class="fh5co-section">
+		<div class="container">
+			<div class="row">
+				<div><h1 align="center">Y aun hay mas!</h1></div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
+					<div class="fh5co-item animate-box">
+						<img src="images/delice.png" class="img-responsive">
+						<h3>Bake Potato Pizza</h3>
+						<span class="fh5co-price">$20<sup>.50</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+					<div class="fh5co-item animate-box">
+						<img src="images/mandms.png" class="img-responsive" >
+						<h3>Bake Potato Pizza</h3>
+						<span class="fh5co-price">$20<sup>.50</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
+					<div class="fh5co-item margin_top animate-box">
+						<img src="images/mandmsamarillo.png" class="img-responsive" >
+						<h3>Sed Fried Chicken</h3>
+						<span class="fh5co-price">$19<sup>.00</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+					<div class="fh5co-item animate-box">
+						<img src="images/mazapanchocolate.png" class="img-responsive" >
+						<h3>Bake Potato Pizza</h3>
+						<span class="fh5co-price">$20<sup>.50</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+				</div>
+				<div class="clearfix visible-sm-block visible-xs-block"></div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
+					<div class="fh5co-item animate-box">
+						<img src="images/clorets_2s.png" class="img-responsive" >
+						<h3>Italian Sauce Mushroom</h3>
+						<span class="fh5co-price">$17<sup>.99</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+					<div class="fh5co-item animate-box">
+						<img src="images/cococuadro.png" class="img-responsive" >
+						<h3>Bake Potato Pizza</h3>
+						<span class="fh5co-price">$20<sup>.50</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
+					<div class="fh5co-item margin_top animate-box">
+						<img src="images/cremino.png" class="img-responsive" >
+						<h3>Fried Potato w/ Garlic</h3>
+						<span class="fh5co-price">$22<sup>.50</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 fh5co-item-wrap">
+					<div class="fh5co-item animate-box">
+						<img src="images/bibipolvo.png" class="img-responsive">
+						<h3>Bake Potato Pizza</h3>
+						<span class="fh5co-price">$20<sup>.50</sup></span>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos nihil cupiditate ut vero alias quaerat inventore molestias vel suscipit explicabo.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 	<div id="fh5co-featured-testimony" class="fh5co-section">
 		<div class="container">
 			<div class="row">
@@ -190,7 +257,7 @@
 				</div>
 
 				<div class="col-md-5 animate-box img-to-responsive">
-						<img src="images/person_1.jpg" alt="">
+						<img src="images/person_1.jpg" >
 				</div>
 				<div class="col-md-7 animate-box">
 					<blockquote>
